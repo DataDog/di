9f93f0f5538af77e814a9a7a1dcaba6f6eec4284
Ofek Lev <ofekmeister@gmail.com>
2018-02-12 01:00:03 -0500

    add executable paths

--- di/agent.py
+++ di/agent.py
@@ -1,2 +1,5 @@
 A5_CONF_DIR = '/etc/dd-agent/conf.d'
+A5_EXE_PATH = '/opt/datadog-agent/agent/agent.py'
+
 A6_CONF_DIR = '/etc/datadog-agent/conf.d'
+A6_EXE_PATH = '/opt/datadog-agent/bin/agent/agent'
