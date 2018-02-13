b095ed22c0dc106832b07f94c4f8cb492b63c8b9
Ofek Lev <ofekmeister@gmail.com>
2018-02-13 12:53:03 -0500

    add checks dir just in case

--- di/agent.py
+++ di/agent.py
@@ -1,5 +1,7 @@
+A5_CHECK_DIR = '/etc/dd-agent/checks.d'
 A5_CONF_DIR = '/etc/dd-agent/conf.d'
 A5_EXE_PATH = '/opt/datadog-agent/agent/agent.py'
 
+A6_CHECK_DIR = '/etc/datadog-agent/checks.d'
 A6_CONF_DIR = '/etc/datadog-agent/conf.d'
 A6_EXE_PATH = '/opt/datadog-agent/bin/agent/agent'
