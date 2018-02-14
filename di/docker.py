d59a4c9def7bbec6a796322a341149bc60616833
Ofek Lev <ofekmeister@gmail.com>
2018-02-14 02:15:03 -0500

    allow exec too

--- di/docker.py
+++ di/docker.py
@@ -8,9 +8,13 @@ from di.utils import NEED_SUBPROCESS_SHELL, get_agent_exe_path, get_check_dir
 __API_KEY = 'a' * 32
 
 
-def run_check(container, check, agent_version_major):
+def run_check(container, check, agent_version_major=None):
+    exe_path = (
+        get_agent_exe_path(agent_version_major) if agent_version_major
+        else get_agent_version(container, running=True)
+    )
     output = subprocess.check_output([
-        'docker', 'exec', container, get_agent_exe_path(agent_version_major), 'check', check
+        'docker', 'exec', container, exe_path, 'check', check
     ], shell=NEED_SUBPROCESS_SHELL).decode().strip()
 
     return output
@@ -24,26 +28,43 @@ def pip_install_mounted_check(container, check):
     return output
 
 
-def get_agent_version(image):
-    version = subprocess.check_output([
-        'docker', 'run', '-e', 'DD_API_KEY={ak}'.format(ak=__API_KEY), image,
-        'head', '--lines=1', '/opt/datadog-agent/version-manifest.txt'
-    ], shell=NEED_SUBPROCESS_SHELL).decode().strip().split()[-1]
+def get_agent_version(image_or_container, running=False):
+    if running:
+        version = subprocess.check_output([
+            'docker', 'exec', image_or_container, 'head', '--lines=1', '/opt/datadog-agent/version-manifest.txt'
+        ], shell=NEED_SUBPROCESS_SHELL).decode().strip().split()[-1]
 
-    if not version[0].isdigit():
-        if dir_exists(os.path.join(A6_CONF_DIR, 'disk'), image):
-            version = '6'
-        else:
-            version = '5'
+        if not version[0].isdigit():
+            if dir_exists(os.path.join(A6_CONF_DIR, 'disk'), image_or_container):
+                version = '6'
+            else:
+                version = '5'
+    else:
+        version = subprocess.check_output([
+            'docker', 'run', '-e', 'DD_API_KEY={ak}'.format(ak=__API_KEY), image_or_container,
+            'head', '--lines=1', '/opt/datadog-agent/version-manifest.txt'
+        ], shell=NEED_SUBPROCESS_SHELL).decode().strip().split()[-1]
+
+        if not version[0].isdigit():
+            if dir_exists(os.path.join(A6_CONF_DIR, 'disk'), image_or_container):
+                version = '6'
+            else:
+                version = '5'
 
     return version
 
 
-def dir_exists(d, image):
-    output = subprocess.check_output([
-        'docker', 'run', '-e', 'DD_API_KEY={ak}'.format(ak=__API_KEY), image,
-        'python', '-c', "import os;print(os.path.isdir('{d}'))".format(d=d)
-    ], shell=NEED_SUBPROCESS_SHELL).decode().strip()
+def dir_exists(d, image_or_container, running=False):
+    if running:
+        output = subprocess.check_output([
+            'docker', 'exec', image_or_container, 'python', '-c',
+            "import os;print(os.path.isdir('{d}'))".format(d=d)
+        ], shell=NEED_SUBPROCESS_SHELL).decode().strip()
+    else:
+        output = subprocess.check_output([
+            'docker', 'run', '-e', 'DD_API_KEY={ak}'.format(ak=__API_KEY), image_or_container,
+            'python', '-c', "import os;print(os.path.isdir('{d}'))".format(d=d)
+        ], shell=NEED_SUBPROCESS_SHELL).decode().strip()
 
     return output == 'True'
 
