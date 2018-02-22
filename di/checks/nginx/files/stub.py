51d75324ed1f0f65eba6460e1881fd11b672d7da
Ofek Lev <ofekmeister@gmail.com>
2018-02-21 23:02:58 -0500

    put files in a package

---
+++ di/checks/nginx/files/stub.py
@@ -0,0 +1,43 @@
+COMPOSE_YAML = """\
+version: '3'
+
+services:
+  nginx:
+    image: nginx:{version}
+    volumes:
+        - {status_path}:/etc/nginx/conf.d/status.conf
+
+  agent:
+    image: {image}
+    container_name: {container_name}
+    environment:
+        {api_key}
+    volumes:
+        {conf_mount}
+        {check_mount}
+    links:
+        - nginx
+"""
+
+STATUS_CONF = """\
+server {
+  listen 81;
+  server_name nginx;
+
+  access_log off;
+  allow all;
+
+  location /nginx_status {
+    # Choose your status module
+
+    # freely available with open source NGINX
+    stub_status;
+
+    # for open source NGINX < version 1.7.5
+    # stub_status on;
+
+    # available only with NGINX Plus
+    # status;
+  }
+}
+"""
